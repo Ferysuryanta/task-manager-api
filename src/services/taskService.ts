7384e86c54ba2e9e25e35da09035974cc9737803
xx-freedom-xx <107397075+Ferysuryanta@users.noreply.github.com>
2025-10-04 23:52:24 +0700

    first commit master

--- src/services/taskService.ts
+++ src/services/taskService.ts
@@ -1,11 +1,20 @@
 import { PrismaClient } from "@prisma/client";
+import {format} from "date-fns";
 
 const prisma = new PrismaClient();
 
 export const taskService = {
     create: async (userId: string, data: any) => {
+        const dueDate = new Date(data.dueDate);
+        const dueDateStr = format(dueDate, "dd-MM-yyyy");
         return prisma.task.create({
-            data: { ...data, userId },
+            data: {
+                title: data.title,
+                description: data.description,
+                dueDate,
+                dueDateStr,
+                userId: userId
+            },
         });
     },
 
